3e9a2456630ec213016b04ce778f3097e0bfdbd3
bebetterthinker <www.thinkbetterthanyouguess@gmail.com>
2023-09-13 23:38:08 +0300

    create the header section of the website

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,23 +1,24 @@
-import Header from '@/components/Header'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from "@/components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'IMDB CLONE',
-  description: 'This is the imdb clone website ',
-}
+  title: "IMDB CLONE",
+  description: "This is the imdb clone website ",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-     {/* header section*/}
-      <Header/>
-      {/*Navbar section*/}
-      {/*Search section */}
-      {children}</body>
+        {/* header section*/}
+        <Header />
+        {/*Navbar section*/}
+        {/*Search section */}
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-   <h1 className='text-red-600'>HOME</h1>
-   </div>
-  )
+    <div>
+      <h1 className="text-red-600">HOME</h1>
+    </div>
+  );
 }

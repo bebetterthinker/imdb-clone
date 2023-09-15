ee5feb566cd5b659994022c637121a36811bd687
bebetterthinker <www.thinkbetterthanyouguess@gmail.com>
2023-09-15 10:48:14 +0300

    featching data using the thembi api

---
+++ src/components/Results.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Results = ({results}) => {
+  return (
+    <div>
+    {results.map((result)=>{
+        return(
+            <div key={result.id}>
+            {result.original_title}
+            </div>
+        )
+    })}
+    </div>
+  )
+}
+
+export default Results
